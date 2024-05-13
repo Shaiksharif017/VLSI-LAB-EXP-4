509026b7915a9c75cc77854d3013fe9edcfb7c49
Shaiksharif017 <167750598+Shaiksharif017@users.noreply.github.com>
2024-05-13 14:13:18 +0530

    Update Exp4_Mod10counter.v

--- Exp4_Mod10counter.v
+++ Exp4_Mod10counter.v
@@ -1,12 +1,21 @@
-//MOD -10 COUNTER 
-module mod10(rst,clk,out); 
-input rst,clk; 
-output reg[3:0]out=4'b0000;
- always@ (posedge clk) 
-begin 
-if(rst==1 | out==4'b1001) 
-out=4'b0000; 
-else 
-out=out+4'b0001;
- end 
+module mod(clk,rst,count);
+
+input clk,rst;
+
+output reg[3:0]count;
+
+always @(posedge clk)
+
+begin
+
+if(rst==1 | count==4'b1001)
+
+count <= 4'b0000;
+
+else
+
+count <= count +1;
+
+end
+
 endmodule
