c195ffae203aaca9e0058c2d40a5673d4cc80dd9
Shaiksharif017 <167750598+Shaiksharif017@users.noreply.github.com>
2024-05-13 14:14:03 +0530

    Update Exp4_Ripplecarrycounter.v

--- Exp4_Ripplecarrycounter.v
+++ Exp4_Ripplecarrycounter.v
@@ -1,28 +1,53 @@
-module ripple_carry_counter(q, clk, reset);
-output [3:0] q;
-input clk, reset;
-T_FF tff0(q[0], clk, reset);
-T_FF tff1(q[1], q[0], reset);
-T_FF tff2(q[2], q[1], reset);
-T_FF tff3(q[3], q[2], reset);
+module ripplecounter(clk,rst,q);
+
+input clk,rst;
+
+output [3:0]q;
+
+tff tff1(q[0],clk,rst);
+
+tff tff2(q[1],q[0],rst);
+
+tff tff3(q[2],q[1],rst);
+
+tff tff4(q[3],q[2],rst);
+
 endmodule
 
-module T_FF(q, clk, reset);
+module tff(q,clk,rst);
+
+input clk,rst;
+
 output q;
-input clk, reset;
+
 wire d;
-D_FF dff0(q, d, clk, reset);
-not n1(d, q); 
+
+dff df1(q,d,clk,rst);
+
+not n1(d,q);
+
 endmodule
 
-module D_FF(q, d, clk, reset);
+module dff(q,d,clk,rst);
+
+input d,clk,rst;
+
 output q;
-input d, clk, reset;
-reg q;
-always @(posedge reset or negedge clk)
-if (reset)
-q = 1'b0;
-else
-q = d;
-endmodule
 
+reg q;
+
+always@(posedge clk or posedge rst)
+
+begin
+
+if(rst)
+
+q=1'b10;
+
+else
+
+q=d;
+
+end
+
+endmodule
